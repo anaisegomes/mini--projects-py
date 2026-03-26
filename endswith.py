6f54811a2659cff1afbe89da05791e30a6ab9b0b
anaisegomes <naiise321@gmail.com>
2026-03-26 20:22:57 -0300

    Create endswith.py

---
+++ endswith.py
@@ -0,0 +1,9 @@
+
+# endswith metodo que verifica se a string fornecida termina com o 
+# argumento especificado e retorna TRue ou False
+
+
+if 'epsilon'.endswith('on'):
+    print("yes")
+else:
+    print('no')
